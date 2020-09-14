d3dc636018c55e5f2944dba5cd9d6f55e803496a
Justin Perez <justin@startrev.com>
2020-09-14 00:44:16 -0700

    Update dotenv path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: 'variables.env' })
+require('dotenv').config({ path: '.env' }) // Load environment variables
 
 const express = require('express')
 const bodyParser = require('body-parser')
