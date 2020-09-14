6323a900eb6e511c487a039b8e1d974739fa774c
Justin Perez <justin@startrev.com>
2020-09-13 20:09:11 -0700

    Setup the endpoint that will receive events from Facebook messenger

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const verifyWebhook = require('./src/verify-webhook')
+
 const app = express()
 
 app.use(bodyParser.json()) // Process application/json
 app.use(bodyParser.urlencoded({ extended: false })) // Process application/x-www-form-urlencoded
 
 app.get('/', (req, res) => res.send('Hello!')) // Index route
+
+app.get('/webhook', verifyWebhook) // Facebook verification
 
 app.listen(5000, () => console.log('⚡ listening on port 5000')) // Listen (start server with node index.js)
